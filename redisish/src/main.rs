c2d29cec6786516cda6c8c482ac09f518cbdea98
Artyom Kazak <yom@artyom.me>
2018-10-12 12:00:32 +0200

    Async

--- redisish/src/main.rs
+++ redisish/src/main.rs
@@ -10,38 +10,46 @@ use parse::*;
 
 use std::io;
 use std::io::{BufRead, BufReader, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7000")?;
-    let mut store = Store::new();
-
-    // Accept connections and process them serially
+    let store = Arc::new(Mutex::new(Store::new()));
     for stream in listener.incoming() {
         let mut stream = stream?;
-        let maybe_cmd = {
-            let mut reader = BufReader::new(&stream);
-            let mut line = String::new();
-            let _ = reader.read_line(&mut line)?;
-            parse(line)
-        };
-        match maybe_cmd {
-            Ok(cmd) => match exec(cmd, &mut store) {
-                Ok(res) => {
-                    stream.write(res.as_bytes()).unwrap();
-                }
-                Err(e) => {
-                    stream
-                        .write(format!("Execution error: {}", e).as_bytes())
-                        .unwrap();
-                }
-            },
-            Err(e) => {
-                stream
-                    .write(format!("Parsing error: {}", e).as_bytes())
-                    .unwrap();
-            }
-        }
+        let store = store.clone();
+        thread::spawn(move || {
+            handle_client(&mut stream, store);
+        });
     }
     Ok(())
+}
+
+fn handle_client(stream: &mut TcpStream, store: Arc<Mutex<Store>>) {
+    let maybe_cmd = {
+        let mut reader = BufReader::new(stream.by_ref());
+        let mut line = String::new();
+        let _ = reader.read_line(&mut line);
+        parse(line)
+    };
+    let mut store = store.lock().unwrap();
+    match maybe_cmd {
+        Ok(cmd) => match exec(cmd, &mut store) {
+            Ok(res) => {
+                stream.write(res.as_bytes()).unwrap();
+            }
+            Err(e) => {
+                stream
+                    .write(format!("Execution error: {}", e).as_bytes())
+                    .unwrap();
+            }
+        },
+        Err(e) => {
+            stream
+                .write(format!("Parsing error: {}", e).as_bytes())
+                .unwrap();
+        }
+    }
 }
